3a671a5df5629ba37c093dc21cca42fdb7b678d5
bleyfer <yfcano3@misena.edu.co>
2024-07-28 20:05:53 -0500

    ARL y errores

--- src/pages/arl/View.vue
+++ src/pages/arl/View.vue
@@ -1,48 +1,51 @@
 <template>
-  <q-card class="q-ma-lg">
-    <q-table flat bordered :rows="arl" :columns="columns" :filter="filter" no-data-label="No se encontraron resultados"
-      no-results-label="No se encontraron resultados en la busqueda" row-key="name">
-      <template v-slot:top-left>
-        <q-btn outline color="primary" icon="mdi-plus" label="Crear" :to="{ name: 'arlCreate' }" />
-      </template>
-      <template v-slot:top-right>
-        <q-input clearable dense debounce="300" outlined v-model="filter" placeholder="Buscar">
-          <template v-slot:prepend>
-            <q-icon name="search" />
-          </template>
-        </q-input>
-      </template>
-      <template v-slot:no-data="{ icon, message, filter }">
-        <div class="full-width row flex-center q-gutter-sm">
-          <span>
-            {{ message }}
-          </span>
-          <q-icon size="2em" :name="filter ? 'mdi-text-box-search' : icon" />
-        </div>
-      </template>
-      <template v-slot:body="props">
-        <q-tr :props="props">
-          <q-td key="name" :props="props">
-            {{ props.row.nombre }}
-          </q-td>
-          <q-td key="actions" :props="props">
-            <div class="q-gutter-sm">
-              <q-btn round outline color="primary" icon="mdi-pencil" size="sm" @click="EditArl(props.row.id)">
-                <q-tooltip class="bg-primary" :offset="[8, 8]" anchor="top middle" self="bottom middle">
-                  Editar
-                </q-tooltip>
-              </q-btn>
-              <q-btn round outline color="red" icon="mdi-delete" @click="alertDialog = true; itemId = props.row.id"
-                size=sm>
-                <q-tooltip class="bg-red mobile-hide" :offset="[8, 8]" anchor="top middle" self="bottom middle">
-                  Eliminar Registro
-                </q-tooltip>
-              </q-btn>
-            </div>
-          </q-td>
-        </q-tr>
-      </template>
-    </q-table>
+  <q-card class="q-ma-lg q-pa-lg">
+    <div class="text-h6">Listado de ARL</div>
+    <div>
+      <q-table flat bordered separator="cell" :rows="arl" :columns="columns" :filter="filter" no-data-label="No se encontraron resultados"
+        no-results-label="No se encontraron resultados en la busqueda" row-key="name">
+        <template v-slot:top-left>
+          <q-btn outline color="primary" icon="mdi-plus" label="Crear" :to="{ name: 'arlCreate' }" />
+        </template>
+        <template v-slot:top-right>
+          <q-input clearable dense debounce="300" outlined v-model="filter" placeholder="Buscar">
+            <template v-slot:prepend>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </template>
+        <template v-slot:no-data="{ icon, message, filter }">
+          <div class="full-width row flex-center q-gutter-sm">
+            <span>
+              {{ message }}
+            </span>
+            <q-icon size="2em" :name="filter ? 'mdi-text-box-search' : icon" />
+          </div>
+        </template>
+        <template v-slot:body="props">
+          <q-tr :props="props">
+            <q-td key="name" :props="props">
+              {{ props.row.nombre }}
+            </q-td>
+            <q-td key="actions" :props="props">
+              <div class="q-gutter-sm">
+                <q-btn round outline color="primary" icon="mdi-pencil" size="sm" @click="EditArl(props.row.id)">
+                  <q-tooltip class="bg-primary" :offset="[8, 8]" anchor="top middle" self="bottom middle">
+                    Editar
+                  </q-tooltip>
+                </q-btn>
+                <q-btn round outline color="red" icon="mdi-delete" @click="alertDialog = true; itemId = props.row.id"
+                  size=sm>
+                  <q-tooltip class="bg-red mobile-hide" :offset="[8, 8]" anchor="top middle" self="bottom middle">
+                    Eliminar Registro
+                  </q-tooltip>
+                </q-btn>
+              </div>
+            </q-td>
+          </q-tr>
+        </template>
+      </q-table>
+    </div>
   </q-card>
 
   <!-- Componete MyAlertDialog -->
